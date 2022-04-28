d7c226820c89be3365f7c3ab7fa0d08a211420ca
raushankumar1511 <103742785+raushankumar1511@users.noreply.github.com>
2022-04-29 02:02:09 +0530

    Add files via upload

--- pattern32.java
+++ pattern32.java
@@ -1,34 +1,54 @@
 public class pattern32 {
-   public static void main(String[] args) {
-       int n=5;
-       int nst=1;
-       int val=1;
-       int row=1;
-       while(row<=2*n-1){
-           int cst=1;
-          int cval=val;
-           while(cst<=nst){
-               if(cst%2==0){
-                   System.out.print("* ");
-               }else{
-
-                   System.out.print(cval+" ");
-               }
-             cst+=1;
-           }
-           if(row<n){
-
-               nst+=2;
-               val+=1;
-               
-            }else{
-                nst-=2;
-                val-=1;
-           }
-           row+=1;
-           System.out.println();
-        }
-       
-   }
     
+        public static void main(String[] args) {
+                // TODO Auto-generated method stub
+                int n = 10;
+        
+                int row = 1;
+                int nsp = n-1;
+                int nst = 1;
+                int val = 10;
+        
+                while(row <= n)
+                {
+                    //Work
+                    int csp = 1;
+                    while(csp <= nsp)
+                    {
+                        System.out.print("  ");
+                        csp += 1;
+                    }
+        
+                    int cst = 1;
+                    int cval = val;
+                    while(cst <= nst)
+                    {
+                        if(cst==nst/2+1){
+                        System.out.print(0+" ");
+                        }else{
+                        System.out.print(cval+" ");
+                            
+                        }
+                        if(cst <(nst/2)+1){
+                            cval += 1;
+                            
+                        }
+                        else{
+                            cval -= 1;
+                        }
+                        cst += 1;
+                    }
+        
+                    //Update
+                //	cval+=1;
+                    nsp -= 1;
+                    nst += 2;
+                    row += 1;
+        //			val  = cval;
+                    val -= 1;
+                    System.out.println();
+                
+        
+            }
+        }
 }
